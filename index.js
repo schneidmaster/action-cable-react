0a68c4d19f1301de35cae807d00ff946288b6bda
Zach Schneider <zach@schneid.io>
2015-12-20 15:43:22 -0600

    remove accidental semicolon

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = {
   ActionCable: require('./dist/action_cable.min'),
   Cable: require('./dist/cable.min'),
   CableMixin: require('./dist/cable_mixin.min'),
-  ChannelMixin: require('./dist/channel_mixin.min');
+  ChannelMixin: require('./dist/channel_mixin.min'),
   version: require('./version')
 };
